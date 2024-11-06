cec3b9540c021524614b910f2e593fb9988fecc0
Fassio Blatter <fassio@specpose.com>
2024-11-06 07:35:30 +0100

    Using git submodules for external libraries

--- ext/build-eigen.sh
+++ ext/build-eigen.sh
@@ -4,10 +4,7 @@ set -e
 
 CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_SHARED_LIBS=0"
 
-mkdir -p src
-if [ ! -d src/eigen ]; then
-    git clone https://gitlab.com/libeigen/eigen.git src/eigen -b 3.4.0
-fi
+git submodule update src/eigen
 
 cmake $CMAKE_FLAGS -B build/eigen src/eigen -DCMAKE_INSTALL_PREFIX="$PWD/dist/eigen"
 cmake --build build/eigen --target install
