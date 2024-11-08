dc7f6d6ee882ea18c2fde1958d5e36d2ccbf5bd3
Fassio Blatter <fassio@specpose.com>
2024-11-08 22:09:14 +0100

    cmake add_subdirectory based build for examples, Eigen3 and SDL

--- build-external-libraries.sh
+++ build-external-libraries.sh
@@ -2,16 +2,14 @@
 
 set -e
 
+mkdir -p ext/src
+if [ ! -d ext/src/eigen ]; then
+    git clone https://gitlab.com/libeigen/eigen.git ext/src/eigen -b 3.4.0
+fi
+
 export CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release $CMAKE_FLAGS"
 
-cd ext
-./build-eigen.sh
-./build-sdl.sh
-./build-opencv.sh
-
-if  [ "$(uname -o)" == "Msys" ]; then
-    echo "Not building boost and gmp on windows due to some difficulties"
-else
-    ./build-boost.sh
-    ./build-gmp.sh
-fi
+cmake $CMAKE_FLAGS -B ext/build ext
+cmake --build ext/build
+#There is a install target for a header-only lib
+cmake --install ext/build/eigen --prefix ext/dist/eigen
