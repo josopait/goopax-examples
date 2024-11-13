51b91a64ad7f400680cc0f60a152a60a1b770f03
Fassio Blatter <fassio@specpose.com>
2024-11-13 14:28:54 +0100

    https://developer.android.com/studio/projects/configure-cmake

--- build.sh
+++ build.sh
@@ -2,21 +2,32 @@
 
 set -e
 
+export ABI="x86_64"
+export platform_version_string="android-31"
+export android_sdk="$HOME/Android/Sdk"
+export ndk_version="27.2.12479018"
+
 export goopax_DIR="$PWD/.."
-export SDL2_DIR=ext/dist/sdl2
+#export SDL2_DIR=ext/dist/sdl2
 export Eigen3_DIR=ext/dist/eigen
-export OpenCV_DIR=ext/dist/opencv
-export Boost_DIR=ext/dist/boost
-export CMAKE_PREFIX_PATH="$PWD/ext/dist/gmp"
+#export OpenCV_DIR=ext/dist/opencv
+#export Boost_DIR=ext/dist/boost
+#export CMAKE_PREFIX_PATH="$PWD/ext/dist/gmp"
 
 if  [ "$(uname -o)" == "Msys" ]; then
 	CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug" -DOpenCV_STATIC=ON
 fi
 
-cmake $CMAKE_FLAGS -B build src -DCMAKE_INSTALL_PREFIX="$PWD/dist"
+#requires cmake>=3.21
+$HOME/Android/Sdk/cmake/3.22.1/bin/cmake $CMAKE_FLAGS -B build/debug/$ABI -G "Ninja" -H src -DCMAKE_SYSTEM_NAME="Android" -DANDROID_ABI="$ABI" -DANDROID_PLATFORM="$platform_version_string" -DANDROID_NDK="$android_sdk/ndk/$ndk_version" -DCMAKE_TOOLCHAIN_FILE="$android_sdk/ndk/$ndk_version/build/cmake/android.toolchain.cmake" -DCMAKE_INSTALL_PREFIX="$PWD/dist"
 
 if (echo "$CMAKE_FLAGS" | grep CMAKE_SYSTEM_NAME=iOS); then
     echo "iOS. Skipping install. Please open build/goopax_examples.xcodeproj and build manually."
 else
-    cmake --build build --target install
+    $HOME/Android/Sdk/cmake/3.22.1/bin/cmake --build build --target install
 fi
+
+unset -v ABI
+unset -v platform_version_string
+unset -v android_sdk
+unset -v ndk_version
--- ext/build-eigen.sh
+++ ext/build-eigen.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+export ABI="x86_64"
+export platform_version_string="android-31"
+export android_sdk="$HOME/Android/Sdk"
+export ndk_version="27.2.12479018"
+
 CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_SHARED_LIBS=0"
 
 mkdir -p src
@@ -9,5 +14,11 @@ if [ ! -d src/eigen ]; then
     git clone https://gitlab.com/libeigen/eigen.git src/eigen -b 3.4.0
 fi
 
-cmake $CMAKE_FLAGS -B build/eigen src/eigen -DCMAKE_INSTALL_PREFIX="$PWD/dist/eigen"
-cmake --build build/eigen --target install
+#requires cmake>=3.21
+$HOME/Android/Sdk/cmake/3.22.1/bin/cmake $CMAKE_FLAGS -B build/eigen/debug/$ABI -G "Ninja" -H src/eigen -DCMAKE_SYSTEM_NAME="Android" -DANDROID_ABI="$ABI" -DANDROID_PLATFORM="$platform_version_string" -DANDROID_NDK="$android_sdk/ndk/$ndk_version" -DCMAKE_TOOLCHAIN_FILE="$android_sdk/ndk/$ndk_version/build/cmake/android.toolchain.cmake" -DCMAKE_INSTALL_PREFIX="$PWD/dist/eigen"
+$HOME/Android/Sdk/cmake/3.22.1/bin/cmake --build build/eigen/debug/$ABI --target install
+
+unset -v ABI
+unset -v platform_version_string
+unset -v android_sdk
+unset -v ndk_version
