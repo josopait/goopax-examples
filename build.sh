1bcc0353c60f9181b46139b720b64bda71bd27c8
Fassio Blatter <fassio@specpose.com>
2024-11-27 11:12:37 +0100

    WINDOWS fixed static build-external-libraries.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export Boost_DIR=ext/dist/boost
 export CMAKE_PREFIX_PATH="$PWD/ext/dist/gmp"
 
 if  [ "$(uname -o)" == "Msys" ]; then
-	CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug" -DOpenCV_STATIC=ON
+	CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug -DOpenCV_STATIC=ON"
 fi
 
 cmake $CMAKE_FLAGS -B build src -DCMAKE_INSTALL_PREFIX="$PWD/dist"
