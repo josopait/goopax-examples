2e9016f72159f14a7942d3481b2315db81af5ab3
Fassio Blatter <fassio@specpose.com>
2024-12-11 20:23:43 +0100

    LINUX: Script for GMP obsolete

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 set -e
 
 export goopax_DIR="$PWD/.."
-export Eigen3_DIR=dist/eigen
-export SDL3_DIR=dist/sdl3
-export OpenCV_DIR=dist/opencv
+export Eigen3_DIR=ext/dist/eigen
+export SDL3_DIR=ext/dist/sdl3
+export OpenCV_DIR=ext/dist/opencv
 
 export goopax_DIR="$PWD/.."
 if  [ "$(uname -o)" == "Msys" ]; then
     CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug -DOpenCV_STATIC=ON"
 else
-    export Boost_DIR=dist/boost
-    export CMAKE_PREFIX_PATH="$PWD/ext/dist/gmp"
+    export Boost_DIR=ext/dist/boost
+    export CMAKE_PREFIX_PATH="$PWD/ext/dist/gmp;$PWD/dist/gmp"
 fi
 
 cmake $CMAKE_FLAGS -B build src --install-prefix "$PWD/dist"
