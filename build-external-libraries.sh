8b0534baefbae74113b9fdbdc478b1da18bb44fb
Fassio Blatter <fassio@specpose.com>
2024-12-15 15:57:41 +0100

    Refactoring macros to avoid intermediate targets. Removed missing dependency checks for libs. Added executable dependency checks.

--- build-external-libraries.sh
+++ build-external-libraries.sh
@@ -8,13 +8,13 @@ fi
 export CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release $CMAKE_FLAGS"
 
 cmake $CMAKE_FLAGS -B build src --install-prefix "$PWD/dist"
-./ext/build-eigen.sh
+cmake --build build --target eigen
 cmake --build build --target sdl3
-./ext/build-opencv.sh
+#cmake --build build --target opencv
 
 if  [ "$(uname -o)" == "Msys" ]; then
     echo "Not building boost and gmp on windows due to some difficulties"
 else
     cmake --build build --target boost
-    ./ext/build-gmp.sh
+#    cmake --build build --target gmp_tmp
 fi
--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ set -e
 export goopax_DIR="$PWD/.."
 export Eigen3_DIR=dist/eigen
 export SDL3_DIR=dist/sdl3
-export OpenCV_DIR=dist/opencv
+#export OpenCV_DIR=dist/opencv
 
 if  [ "$(uname -o)" == "Msys" ]; then
     CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug -DOpenCV_STATIC=ON"
 else
     export Boost_DIR=dist/boost
-    export CMAKE_PREFIX_PATH="$PWD/dist/gmp"
+#    export CMAKE_PREFIX_PATH="$PWD/dist/gmp"
 fi
 
 cmake $CMAKE_FLAGS -B build src --install-prefix "$PWD/dist"
