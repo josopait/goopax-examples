1109ceb09fbe90e4d0f3b59a15a247146e30ef6a
Fassio Blatter <fassio@specpose.com>
2025-01-01 11:07:31 +0100

    Merged from josopait/goopax-examples external_package

---
+++ build-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+export CMAKE_BUILD_PARALLEL_LEVEL="$(getconf _NPROCESSORS_ONLN)"
+
+if  [ "$(uname -o)" == "Msys" ]; then
+	CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug" -DOpenCV_STATIC=ON
+fi
+
+cmake $CMAKE_FLAGS -B build src -DCMAKE_INSTALL_PREFIX="$PWD/dist"
+cmake --build build --target build_external_libraries
+cmake build
+cmake --build build
+
+
+if (echo "$CMAKE_FLAGS" | grep CMAKE_SYSTEM_NAME=iOS); then
+    echo "iOS. Skipping install. Please open build/goopax_examples.xcodeproj and build manually."
+else
+    cmake --install build
+fi
