f06c81d617ae0f60418c276d349ab01f71b407dd
Fassio Blatter <fassio@specpose.com>
2025-01-14 22:05:47 +0100

    Fixed cmake configure

--- build-for-android.sh
+++ build-for-android.sh
@@ -6,10 +6,10 @@ source ./android-ninja-config.sh
 
 #export goopax_DIR="$PWD/../goopax-5.6.1-Android-aarch64"
 #export CMAKE_PREFIX_PATH="$PWD/../goopax-5.6.1-Android-aarch64/lib"
-CMAKE_FLAGS="$CMAKE_FLAGS -DGOOPAX_DRAW_WITH_OPENGL=0 -DGOOPAX_DRAW_WITH_METAL=0 -DCMAKE_FIND_ROOT_PATH=$PWD/../"
+CMAKE_FLAGS="$CMAKE_FLAGS -DGOOPAX_DRAW_WITH_OPENGL=0 -DGOOPAX_DRAW_WITH_METAL=0 -DCMAKE_FIND_ROOT_PATH=$PWD/../;$PWD/build/debug/arm64-v8a/ext/boost;$PWD/build/debug/arm64-v8a/ext/opencv;$PWD/build/debug/arm64-v8a/ext/sdl3;$PWD/build/debug/arm64-v8a/ext/eigen"
 
 #requires cmake>=3.21
-$HOME/Android/Sdk/cmake/3.22.1/bin/cmake $CMAKE_FLAGS -B build/$build_type/$ABI -G "Ninja" src -DCMAKE_SYSTEM_NAME="Android" -DANDROID_ABI="$ABI" -DANDROID_PLATFORM="$platform_version_string" -DANDROID_NDK="$android_sdk/ndk/$ndk_version" -DCMAKE_TOOLCHAIN_FILE="$android_sdk/ndk/$ndk_version/build/cmake/android.toolchain.cmake" --install-prefix="$PWD/tmp"
+$HOME/Android/Sdk/cmake/3.22.1/bin/cmake $CMAKE_FLAGS -B build/$build_type/$ABI -G "Ninja" src -DCMAKE_SYSTEM_NAME="Android" -DANDROID_ABI="$ABI" -DANDROID_PLATFORM="$platform_version_string" -DANDROID_NDK="$android_sdk/ndk/$ndk_version" -DCMAKE_TOOLCHAIN_FILE="$android_sdk/ndk/$ndk_version/build/cmake/android.toolchain.cmake"
 $HOME/Android/Sdk/cmake/3.22.1/bin/cmake --build build/$build_type/$ABI --target build_eigen
 $HOME/Android/Sdk/cmake/3.22.1/bin/cmake --build build/$build_type/$ABI --target build_sdl3
 $HOME/Android/Sdk/cmake/3.22.1/bin/cmake --build build/$build_type/$ABI --target build_opencv
