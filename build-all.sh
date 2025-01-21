8c3ce0c83499323c637e7f5d7bb9ed096b7abab6
Ingo Josopait <josopait@goopax.com>
2025-01-21 22:12:30 +0100

    adding `#include <SDL3/SDL_main.h>`

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ set -e
 
 export CMAKE_BUILD_PARALLEL_LEVEL="$(getconf _NPROCESSORS_ONLN)"
 
-cmake "$@" $CMAKE_FLAGS -B build src
+cmake "$@" -B build src
 cmake --build build --target build_external_libraries
 cmake build
 cmake --build build
